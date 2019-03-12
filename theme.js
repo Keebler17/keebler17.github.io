ddea7d320331b012becb33c3e23809a65e4c56ce
Keebler17 <39811002+Keebler17@users.noreply.github.com>
2019-03-12 11:39:34 -0400

    Removed the text change in $(".theme")

--- theme.js
+++ theme.js
@@ -1,11 +1,9 @@
 $(".themeswitch").click(function() {
 	var theme = $(".theme").html();
 	if(theme == "Light") {
-		$(".theme").html("Dark");
 		$("html").css("background-color", "#103166");
 		$(".site").css("color", "#ffffff")
 	} else {
-		$(".theme").html("Light");
 		$("html").css("background-color", "#ffffff");
 		$(".site").css("color", "#000000");
 	}
