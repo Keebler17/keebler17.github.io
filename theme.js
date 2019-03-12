31d1db6f42667d8e7913c7526c8c3c0bdb0ba68d
Keebler17 <39811002+Keebler17@users.noreply.github.com>
2019-03-12 14:16:03 -0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,10 +1,10 @@
 $(".themeswitch").click(function() {
-	var theme = $(".theme").html();
-	if(theme == "Light") {
-		$("html").css("background-color", "#103166");
-		$(".site").css("color", "#ffffff")
-	} else {
+	let bg = $("html").css("color")
+	if(bg == "#103166") {
 		$("html").css("background-color", "#ffffff");
 		$(".site").css("color", "#000000");
+	} else {
+		$("html").css("background-color", "#103166");
+		$(".site").css("color", "#ffffff")
 	}
 });
