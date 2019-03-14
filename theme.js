677141c0638ea56d01fbf77082231e424c90ccdd
Keebler17 <39811002+Keebler17@users.noreply.github.com>
2019-03-14 14:09:44 -0400

    document.write debug

--- theme.js
+++ theme.js
@@ -1,10 +1,11 @@
 function switchTheme() {
-	var bg = $("html").css("color")
+	var bg = $("html").css("color");
+	document.write(bg);
 	if(bg == "#103166") {
 		$("html").css("background-color", "#ffffff");
 		$(".site").css("color", "#000000");
 	} else {
 		$("html").css("background-color", "#103166");
-		$(".site").css("color", "#ffffff")
+		$(".site").css("color", "#ffffff");
 	}
 }
