20eb56d9c4bb395fca7f5d52cdba0aaf9e78fa95
Keebler17 <cheddarperson493@gmail.com>
2020-12-05 15:10:17 -0500

    h

---
+++ school/geometry/line/index.js
@@ -0,0 +1,46 @@
+var slope = 0
+var pointSlope = ""
+
+first = true
+
+var elt
+var calculator
+
+function solve() {
+  x1 = document.getElementById("x1").value
+  x2 = document.getElementById("x2").value
+  y1 = document.getElementById("y1").value
+  y2 = document.getElementById("y2").value
+
+  slope = getSlope(x1, x2, y1, y2)
+  document.getElementById("slope").innerHTML = slope
+  pointSlope = getPointSlope(x1, x2, y1, y2)
+  document.getElementById("pointSlope").innerHTML = pointSlope
+
+  if(first) {
+    elt = document.getElementById('calculator');
+    calculator = Desmos.GraphingCalculator(elt);
+    first = false
+  }
+  calculator.setExpression({ id: 'graph1', latex: pointSlope });
+
+}
+
+
+function getSlope(x1, x2, y1, y2) {
+
+  console.log("(" + y1 + "-" + y2 + ")/(" + x1 + "-" + x2 + ")")
+
+  if(x1-x2 === 0) {
+    return "undefined"
+  }
+
+
+  result = (y1-y2)/(x1-x2)
+
+  return result
+}
+
+function getPointSlope(x1, x2, y1, y2) {
+  return "x-" + x1 + "=" + slope + "(y-" + y1 + ")"
+}
